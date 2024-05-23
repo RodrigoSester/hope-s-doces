2cc9fc86aca6b9f360ae467df7d324595238ad94
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-23 15:24:04 -0300

    feat(register-order): implement a form to set data of order

--- src/components/order/RegisterOrderModal.vue
+++ src/components/order/RegisterOrderModal.vue
@@ -1,6 +1,41 @@
 <template>
   <q-dialog persistent v-model="openDialog">
-    <q-form greedy @validation-error="showErrorToast"> </q-form>
+    <q-card style="width: 700px; max-width: 80vw">
+      <q-card-section>
+        <q-form greedy @validation-error="showErrorToast">
+          <span> Cliente </span>
+          <q-select
+            v-model="personId"
+            use-input
+            input-debounce="500"
+            clearable
+            options-dense
+            outlined
+            hide-bottom-space
+            option-value="id"
+            option-label="name"
+            :loading="loading"
+            :options="personOptions"
+            @virtual-scroll="onScroll"
+          >
+          </q-select>
+        </q-form>
+      </q-card-section>
+      <q-card-actions>
+        <q-btn
+          flat
+          label="Cancelar"
+          color="negative"
+          @click="openDialog = false"
+        />
+        <q-btn
+          flat
+          label="Salvar"
+          color="primary"
+          @click="openDialog = false"
+        />
+      </q-card-actions>
+    </q-card>
   </q-dialog>
 </template>
 
@@ -14,12 +49,16 @@ export default defineComponent({
   },
   data() {
     return {
+      personId: null,
       openDialog: false,
+      loading: false,
+      personOptions: [],
     };
   },
   watch: {
     value(isOpen) {
       this.openDialog = isOpen;
+      console.log("abrindo modal");
     },
   },
   methods: {
@@ -29,6 +68,16 @@ export default defineComponent({
         position: "bottom",
         message: "Erro ao salvar o pedido",
       });
+    },
+    onScroll({ to, ref }) {
+      console.log("LOG: -> onScroll -> to, ref:", to, ref);
+      setTimeout(() => {
+        nextPage.value++;
+        nextTick(() => {
+          console.log("loading more options");
+          loading.value = false;
+        });
+      }, 500);
     },
   },
 });

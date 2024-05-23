7475621289877e324e15b47cad8d9425d3276ef1
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-23 14:15:46 -0300

    feat: add TODO

--- src/pages/OrderPage.vue
+++ src/pages/OrderPage.vue
@@ -252,6 +252,7 @@ export default defineComponent({
         this.rows = response;
         console.log("LOG: -> fetchData -> this.rows:", response);
       } catch (err) {
+        // TODO: Add toast
         console.error(err);
       } finally {
         this.loading = false;
@@ -259,15 +260,19 @@ export default defineComponent({
     },
 
     onEdit(item) {
+      // TODO: Implementar ação de edição
       console.log("LOG: -> onEdit -> item:", item);
     },
     onSetAsPaid(item) {
+      // TODO: Implementar ação de pagamento
       console.log("LOG: -> handleItemAction -> item:", item);
     },
     onSetAsDelivered(item) {
+      // TODO: Implementar ação de entrega
       console.log("LOG: -> handleItemAction -> item:", item);
     },
     onCancel(item) {
+      // TODO: Implementar ação de cancelamento
       console.log("LOG: -> handleItemAction -> item:", item);
     },
   },

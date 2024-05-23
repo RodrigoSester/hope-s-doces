c10fafe5f627cccf5932e5a9b06e8d66ef099c57
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-23 14:59:44 -0300

    feat(register-order-modal): implement dialog

---
+++ src/components/order/RegisterOrderModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-dialog persistent v-model="openDialog">
+    <q-form greedy @validation-error="showErrorToast"> </q-form>
+  </q-dialog>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RegisterOrderModal",
+  props: {
+    value: Boolean,
+  },
+  data() {
+    return {
+      openDialog: false,
+    };
+  },
+  watch: {
+    value(isOpen) {
+      this.openDialog = isOpen;
+    },
+  },
+  methods: {
+    showErrorToast() {
+      this.$q.notify({
+        color: "negative",
+        position: "bottom",
+        message: "Erro ao salvar o pedido",
+      });
+    },
+  },
+});
+</script>
+
+<style lang="scss"></style>

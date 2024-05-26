1818d711d5ba95dd1d9199f4e7a48f52d777b51b
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-26 18:05:27 -0300

    feat(register-order): implement inputs to set description and price of order

--- src/components/order/RegisterOrderModal.vue
+++ src/components/order/RegisterOrderModal.vue
@@ -3,37 +3,47 @@
     <q-card style="width: 700px; max-width: 80vw">
       <q-card-section>
         <q-form greedy @validation-error="showErrorToast">
-          <span> Cliente </span>
-          <q-select
-            v-model="personId"
-            use-input
-            input-debounce="500"
-            clearable
-            options-dense
-            outlined
-            hide-bottom-space
-            option-value="id"
-            option-label="name"
-            :loading="loading"
-            :options="personOptions"
-            @virtual-scroll="onScroll"
-          >
-          </q-select>
+          <div class="row">
+            <div class="column col-8">
+              <span> Cliente </span>
+              <q-select
+                v-model="personId"
+                use-input
+                input-debounce="500"
+                clearable
+                options-dense
+                outlined
+                hide-bottom-space
+                option-value="id"
+                option-label="name"
+                :loading="loading"
+                :options="personOptions"
+                @virtual-scroll="onScroll"
+              />
+            </div>
+
+            <div class="column full-width q-mt-md" cols="12">
+              <span> Descrição do pedido: </span>
+              <q-input v-model="description" outlined autogrow />
+            </div>
+
+            <div class="column">
+              <span> Valor </span>
+              <q-input
+                v-model="price"
+                outlined
+                prefix="R$"
+                mask="#,##"
+                fill-mask="0"
+                reverse-fill-mask
+              />
+            </div>
+          </div>
         </q-form>
       </q-card-section>
-      <q-card-actions>
-        <q-btn
-          flat
-          label="Cancelar"
-          color="negative"
-          @click="openDialog = false"
-        />
-        <q-btn
-          flat
-          label="Salvar"
-          color="primary"
-          @click="openDialog = false"
-        />
+      <q-card-actions class="row justify-end">
+        <q-btn label="Cancelar" @click="openDialog = false" />
+        <q-btn color="primary" label="Salvar" @click="openDialog = false" />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -52,6 +62,8 @@ export default defineComponent({
       personId: null,
       openDialog: false,
       loading: false,
+      price: 0,
+      description: "",
       personOptions: [],
     };
   },

d97cab4e7af7b3af7e9ba40ed2db3dc44560d0e3
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-26 19:14:57 -0300

    src/services/_order.service.js

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -4,5 +4,10 @@ export default {
   async getAll() {
     const response = await api.get('/orders');
     return response.data;
+  },
+
+  async register() {
+    const response = await api.post('/orders');
+    return response.data;
   }
 }

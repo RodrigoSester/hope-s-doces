4c2c0d99f3313166b67972c42afa260b09ff94f5
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-26 18:45:07 -0300

    feat(register-order): implement header, add input for address and date deliver

--- src/components/order/RegisterOrderModal.vue
+++ src/components/order/RegisterOrderModal.vue
@@ -1,11 +1,20 @@
 <template>
-  <q-dialog persistent v-model="openDialog">
+  <q-dialog persistent v-model="openDialog" class="register-order">
     <q-card style="width: 700px; max-width: 80vw">
+      <q-card-title>
+        <div
+          class="q-pa-md row justify-between items-center register-order__header"
+        >
+          <span class="register-order__header-label"> Registrar pedido </span>
+
+          <q-btn flat fab-mini icon="mdi-close" @click="closeModal" />
+        </div>
+      </q-card-title>
       <q-card-section>
         <q-form greedy @validation-error="showErrorToast">
           <div class="row">
             <div class="column col-8">
-              <span> Cliente </span>
+              <span class="register-order__input-label"> Cliente: </span>
               <q-select
                 v-model="personId"
                 use-input
@@ -22,27 +31,54 @@
               />
             </div>
 
-            <div class="column full-width q-mt-md" cols="12">
-              <span> Descrição do pedido: </span>
+            <div class="column full-width q-my-md" cols="12">
+              <span class="register-order__input-label">
+                Descrição do pedido:
+              </span>
               <q-input v-model="description" outlined autogrow />
             </div>
 
-            <div class="column">
-              <span> Valor </span>
-              <q-input
-                v-model="price"
-                outlined
-                prefix="R$"
-                mask="#,##"
-                fill-mask="0"
-                reverse-fill-mask
-              />
+            <div class="row justify-between col-12">
+              <div class="column col-6 q-pr-md">
+                <span class="register-order__input-label"> Valor: </span>
+                <q-input
+                  v-model="price"
+                  outlined
+                  prefix="R$"
+                  mask="#,##"
+                  fill-mask="0"
+                  reverse-fill-mask
+                />
+              </div>
+
+              <div class="column col-6">
+                <span class="register-order__input-label">
+                  Data de entrega:
+                </span>
+                <q-input outlined type="date" />
+              </div>
+            </div>
+
+            <div class="q-mt-md row col-12 justify-between">
+              <div class="column col-6">
+                <span class="register-order__input-label">
+                  Utilizar endereço do cliente?
+                </span>
+                <q-toggle v-model="useClientAddress" />
+              </div>
+
+              <div class="column col-6">
+                <span class="register-order__input-label">
+                  Endereço de entrega:
+                </span>
+                <q-input v-model="address" outlined />
+              </div>
             </div>
           </div>
         </q-form>
       </q-card-section>
-      <q-card-actions class="row justify-end">
-        <q-btn label="Cancelar" @click="openDialog = false" />
+      <q-card-actions class="row justify-end register-order__actions">
+        <q-btn flat label="Cancelar" @click="openDialog = false" />
         <q-btn color="primary" label="Salvar" @click="openDialog = false" />
       </q-card-actions>
     </q-card>
@@ -62,8 +98,10 @@ export default defineComponent({
       personId: null,
       openDialog: false,
       loading: false,
+      useClientAddress: false,
       price: 0,
       description: "",
+      address: "",
       personOptions: [],
     };
   },
@@ -91,8 +129,34 @@ export default defineComponent({
         });
       }, 500);
     },
+    closeModal() {
+      this.openDialog = false;
+      this.$emit("input", false);
+    },
   },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.register-order {
+  &__header {
+    border-bottom: 1px solid $text--base;
+  }
+
+  &__header-label {
+    font-size: 1.5rem;
+    font-family: Fredoka One;
+    color: $text;
+  }
+
+  &__input-label {
+    font-size: 0.75rem;
+    color: $text;
+    font-weight: bold;
+  }
+
+  &__actions {
+    border-top: 1px solid $text--base;
+  }
+}
+</style>

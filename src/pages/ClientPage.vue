b3772c84dfc31c2304841d642d4ff823aacb4bf5
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-06-30 03:56:51 -0300

    feat(client-list): copy page of order to make clients list

---
+++ src/pages/ClientPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <ClientList />
+  </q-page>
+</template>
+
+<script>
+import ClientList from "components/client/ClientList.vue";
+
+export default {
+  components: {
+    ClientList,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,28 +1,29 @@
-
 const routes = [
   {
-    path: '/orders',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/OrderPage.vue') }
-    ],
+    path: "/orders",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/OrderPage.vue") }],
     meta: { requiresAuth: true },
   },
   {
-    path: '/login',
-    component: () => import('layouts/LoginLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/LoginPage.vue') }
-    ],
-    meta: { requiresAuth: false }
+    path: "/clients",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/ClientPage.vue") }],
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/login",
+    component: () => import("layouts/LoginLayout.vue"),
+    children: [{ path: "", component: () => import("pages/LoginPage.vue") }],
+    meta: { requiresAuth: false },
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;

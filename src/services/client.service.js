7aa6572093d22eec16b9efa736685710dab3c27c
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-08-20 19:59:48 -0300

    feat(client): update client service route

--- src/services/client.service.js
+++ src/services/client.service.js
@@ -9,4 +9,8 @@ export default {
     const response = await api.post("/person", body);
     return response.data;
   },
+  async update(body) {
+    const response = await api.put(`/person/${body.clientId}`, body);
+    return response.data;
+  },
 };
